1d06e9542f8d1a49b7098adac29f016ad13f552c
ryosuke-horie <ryosuke03140@gmail.com>
2023-01-02 12:18:03 +0900

    変数

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+// var ステートメントは変数を宣言する。
+// 複数の変数の最後に型を書くことで変数のリストを宣言できる
+var c, python, java bool
+
+// 「0 false false false」？？？？
+func main() {
+	var i int 
+	fmt.PrintIn(i, c, python, java)
+}
